fdb579535747ea6551dc2ed0cd6acf23dff06346
prinsonnazareth2@gmail.com <prinsonnazareth2@gmail.com>
2025-12-21 21:55:57 +0530

    worked on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import { useNavigate } from 'react-router'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-
+import mainprofilepic from '../assets/mainprofilepic.jpg'
 const About = () => {
   const aboutMe = useRef(null)
   
@@ -15,22 +15,31 @@ const About = () => {
   })
 
   return (
-    <div>
-      <div ref={aboutMe} className='flex flex-col min-h-screen w-full px-4 md:px-20 lg:px-40 xl:px-60 overflow-auto hide-scrollbar gap-5'> 
+    <div className='min-h-screen'>
       <Navbar/>
-
+      <div ref={aboutMe} className='h-[calc(100vh-64px)] overflow-y-auto px-4 md:px-20 lg:px-40 xl:px-60 hide-scrollbar'>
         <div className='pt-10 px-4'>
           <h1 className='text-3xl md:text-4xl font-bold text-left'>
             About me
           </h1>
         </div>
-        <div className='px-4 space-y-4'>
-          <p className='text-lg md:text-xl text-left'>
-            I am a developer with a passion for creating clean, user-friendly applications
-          </p>
-          <p className='text-lg md:text-xl text-left'>
-            I love clean code, modern tools, and always learning new things
-          </p>
+        {/* about me info*/}
+          <div className=''>
+            <div>
+              <img 
+              src={mainprofilepic} 
+              alt='profile pic'
+              className='"w-40 h-40 sm:w-60 sm:h-60 rounded-full object-cover shadow-lg'
+               />
+            </div>
+            <div className='px-4 space-y-4'>
+            <p className='text-lg md:text-xl text-left'>
+              I am a developer with a passion for creating clean, user-friendly applications
+            </p>
+            <p className='text-lg md:text-xl text-left'>
+              I love clean code, modern tools, and always learning new things
+            </p>
+          </div>
         </div>
       </div>
     </div>
